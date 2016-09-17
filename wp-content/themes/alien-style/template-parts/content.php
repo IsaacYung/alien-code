2850df54c6dda0ee4f044cade7cc27fbbd3b616e
Isaac Yung Lopes <isaaczoi@gmail.com>
2016-09-17 16:44:58 -0300

    update

--- wp-content/themes/alien-style/template-parts/content.php
+++ wp-content/themes/alien-style/template-parts/content.php
@@ -8,25 +8,26 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'row' ); ?>>
 	<header class="entry-header">
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 			<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
 		<?php endif; ?>
+	</header>
+	<!-- .entry-header -->
+	<div class="row">
+		<div class="thumbnail-container medium-3 columns">
+			<?php alien_post_thumbnail(); ?>
+		</div>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php alien_excerpt(); ?>
-
-	<?php alien_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
+		<div class="entry-content medium-8 columns end">
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<?php alien_excerpt(); ?>
+			<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-				get_the_title()
+			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+			get_the_title()
 			) );
 
 			wp_link_pages( array(
@@ -36,18 +37,18 @@
 				'link_after'  => '</span>',
 				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
 				'separator'   => '<span class="screen-reader-text">, </span>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php alien_entry_meta(); ?>
+				) );
+				?>
+				<?php alien_entry_meta(); ?>
+			</div><!-- .entry-content -->
+	</div>
+	<footer class="entry-footer float-right">
 		<?php
 			edit_post_link(
 				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
+				/* translators: %s: Name of current post */
+				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				get_the_title()
 				),
 				'<span class="edit-link">',
 				'</span>'
